59a2a7c9c31207b4ed64b22fa4e551fef023f307
kingvegeth <kingvegthpc@gmail.com>
2024-02-08 16:47:37 +0100

    Prima commit (fatto fino al primo punto di EXTRA)

---
+++ m1/s3/g4/assets/js/script.js
@@ -0,0 +1,37 @@
+//Variabili globali
+let board = document.querySelector(".board");
+let button = document.querySelector(".button");
+
+let numeroCelle = 76;
+
+//creazione delle celle
+
+//<div class="cell">1</div>
+
+for (let i = 1; i < numeroCelle + 1; i++) {
+  let cella = document.createElement("div");
+  cella.classList.add("cell");
+  cella.innerText = i;
+  board.appendChild(cella);
+}
+
+//estrazione del numero con la pressione di 'button'
+
+let numeriEstratti = [];
+button.addEventListener("click", () => {
+  let cell = document.querySelectorAll(".cell");
+
+  if (numeriEstratti.length == numeroCelle) return;
+  let numero;
+
+  do {
+    numero = Math.ceil(Math.random() * numeroCelle);
+  } while (numeriEstratti.includes(numero));
+  numeriEstratti.push(numero);
+
+  for (let i = 0; i < cell.length; i++) {
+    if (cell[i].innerText == numero) {
+      cell[i].classList.add("selected");
+    }
+  }
+});

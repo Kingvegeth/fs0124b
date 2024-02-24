5c1bd1b5bf641bd189cc0c151cdaf8abd77d29b1
kingvegeth <kingvegthpc@gmail.com>
2024-02-24 13:57:51 +0100

    tooltip terminati

--- m2/s1/g5/assets/js/script.js
+++ m2/s1/g5/assets/js/script.js
@@ -1,27 +1,36 @@
+window.addEventListener("scroll", function () {
+  let scrollThreshold = 310;
+  let scrollH = window.scrollY;
+  let navBar = document.getElementById("nav-bar");
+  let button = this.document.getElementsByClassName("btn");
+  if (scrollH >= scrollThreshold) {
+    navBar.style.backgroundColor = "#fff";
+    button[0].style.backgroundColor = "#1a8917";
+  } else if (scrollH < scrollThreshold) {
+    navBar.style.backgroundColor = "#ffc017";
+    button[0].style.backgroundColor = "#000";
+  }
+});
 
-window.addEventListener('scroll',function(){
-    let scrollThreshold=310
-    let scrollH = window.scrollY;
-    let navBar = document.getElementById('nav-bar')
-    let button = this.document.getElementsByClassName('btn')
-    if (scrollH>= scrollThreshold) {
-        navBar.style.backgroundColor='#fff'
-        button[0].style.backgroundColor='#1a8917'
-    }else if (scrollH<scrollThreshold){
-        navBar.style.backgroundColor='#ffc017'
-        button[0].style.backgroundColor='#000'
-    }
+let author =  document.getElementsByClassName("author");
+let tooltip = document.getElementsByClassName("tooltip");
 
-})
+for (let i = 0; i < author.length; i++) {
+  let followerNumber = Math.ceil(Math.random() * 20);
+  let firstName = document.getElementsByClassName('first-name');
+  let authorName = firstName[i].outerText;
 
+  let imgTag = author[i].querySelector("img");
+  let imgSrc = imgTag.getAttribute("src");
 
-let author = document.getElementsByClassName ('author')
-let authorName = author[0].innerText
-
-let imgTag = author[0].querySelector('img')
-let imgSrc = imgTag.getAttribute('src')
+  let tooltipCode = `
+<div class="author-tooltip">    
+    <img src="${imgSrc}" alt="author thumb">
+    <span>${authorName}</span>
+</div>
+<p>Author</p>
+<p class="followers">${followerNumber}k Followers <button class="btn dyn-btn">Follow</button></p>`;
 
 
-console.log(author[0].innerText);
-console.log(author[0].innerHTML);
-console.log(imgSrc);
+  tooltip[i].innerHTML = tooltipCode;
+}

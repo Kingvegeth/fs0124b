3c8d4003d826ee8197cf635c50d9086c03e7dc1f
kingvegeth <kingvegethpc@gmail.com>
2024-02-24 12:50:11 +0100

    tooltip wip

--- m2/s1/g5/assets/js/script.js
+++ m2/s1/g5/assets/js/script.js
@@ -1,12 +1,9 @@
-//Cambio colore allo scroll
-window.addEventListener('scroll',function(){
-    
-    let scrollThreshold=310 
-    let scrollH = window.scrollY;
 
+window.addEventListener('scroll',function(){
+    let scrollThreshold=310
+    let scrollH = window.scrollY;
     let navBar = document.getElementById('nav-bar')
     let button = this.document.getElementsByClassName('btn')
-    
     if (scrollH>= scrollThreshold) {
         navBar.style.backgroundColor='#fff'
         button[0].style.backgroundColor='#1a8917'
@@ -16,3 +13,15 @@ window.addEventListener('scroll',function(){
     }
 
 })
+
+
+let author = document.getElementsByClassName ('author')
+let authorName = author[0].innerText
+
+let imgTag = author[0].querySelector('img')
+let imgSrc = imgTag.getAttribute('src')
+
+
+console.log(author[0].innerText);
+console.log(author[0].innerHTML);
+console.log(imgSrc);

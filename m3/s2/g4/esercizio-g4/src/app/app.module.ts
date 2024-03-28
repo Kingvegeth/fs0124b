80b4b5c697394c2c59aabde8d8021e8167b61efc
kingvegeth <kingvegthpc@gmail.com>
2024-03-28 16:46:20 +0100

    esercizio finito

--- m3/s2/g4/esercizio-g4/src/app/app.module.ts
+++ m3/s2/g4/esercizio-g4/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FavouritesComponent } from './pages/favourites/favourites.component';
+import { HttpClassInterceptor } from './http-class.interceptor';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { FavouritesComponent } from './pages/favourites/favourites.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: HttpClassInterceptor, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

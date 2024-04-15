fde9c511d6e69e0b50a261c898deeb2a9030b0a9
kingvegeth <kingvegethpc@gmail.com>
2024-04-15 17:19:04 +0200

    commit 17

--- m4/s1/g1/esercizio g1/src/Program.java
+++ m4/s1/g1/esercizio g1/src/Program.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Program {
@@ -19,12 +20,41 @@ public class Program {
             return sc.nextDouble();
     }
 
-    public static void main(String[] args){
-        int n = inputInteger("Inserisci un intero");
-        double d = inputDouble("Inserisci un numero");
-        String s = input("Inserisci un testo qualsiasi");
-        System.out.println(n);
-        System.out.println(d);
-        System.out.println(s);
+    public static int moltiplica(int a, int b){
+        return a * b;
     }
+
+    public static String concatena(String stringa, int n) {
+        return stringa + n;
+    }
+
+    public static String[] inserisciInArray(String[] strArr,String stringa) {
+        String[] nuovoArr = {strArr[0],strArr[1],strArr[2],stringa,strArr[3],strArr[4]};
+        return nuovoArr;
+    }
+
+    public static void stringOrder(String stringa1, String stringa2) {
+        String normale = stringa1 + stringa2;
+        String inverso = stringa2 + stringa1;
+        System.out.println("Concatenazione normale: " + normale);
+        System.out.println("Concatenazione inversa: " + inverso);
+    }
+
+
+    public static void main(String[] args){
+        String[] array = { "primo", "secondo", "terzo", "quarto", "quinto"};
+
+        System.out.println("Esercizio 1 - 1: " + moltiplica(2, 5));
+        System.out.println("Esercizio 1 - 2: " + concatena("pippo", 5));
+        System.out.println("Esercizio 1 - 3: " + Arrays.toString(inserisciInArray(array, "pippo")));
+
+
+        String s1 = input("Inserisci la prima stringa");
+        String s2 = input("Inserisci la seconda stringa");
+        stringOrder(s1, s2);
+
+
+    }
+
+
 }

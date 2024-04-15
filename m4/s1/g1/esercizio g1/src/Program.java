41b71fedf9d46f03473ba15db098f01f8d21e4fe
kingvegeth <kingvegethpc@gmail.com>
2024-04-15 17:50:35 +0200

    commit 17

--- m4/s1/g1/esercizio g1/src/Program.java
+++ m4/s1/g1/esercizio g1/src/Program.java
@@ -29,15 +29,29 @@ public class Program {
     }
 
     public static String[] inserisciInArray(String[] strArr,String stringa) {
-        String[] nuovoArr = {strArr[0],strArr[1],strArr[2],stringa,strArr[3],strArr[4]};
-        return nuovoArr;
+        return new String[]{strArr[0], strArr[1], strArr[2], stringa, strArr[3], strArr[4]};
+
     }
 
     public static void stringOrder(String stringa1, String stringa2) {
         String normale = stringa1 + stringa2;
         String inverso = stringa2 + stringa1;
-        System.out.println("Concatenazione normale: " + normale);
-        System.out.println("Concatenazione inversa: " + inverso);
+        System.out.println("Esercizio 2: Concatenazione normale: " + normale);
+        System.out.println("Esercizio 2: Concatenazione inversa: " + inverso);
+    }
+
+    public static float perimetroRettangolo(float l, float h) {
+        return (l*2) + (h*2);
+    }
+
+    public static int pariDispari(int n){
+        return n % 2;
+    }
+
+    public static float perimetroTriangolo(float a, float b, float c) {
+        float perimetro = a + b + c;
+        float p = perimetro / 2;
+        return (float) Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
 
@@ -53,6 +67,9 @@ public class Program {
         String s2 = input("Inserisci la seconda stringa");
         stringOrder(s1, s2);
 
+        System.out.println("Esercizio 3 - 1: il perimetro è " + perimetroRettangolo(2,4));
+        System.out.println("Esercizio 3 - 2: " + pariDispari(5));
+        System.out.println("Esercizio 3 - 3: l'area è " + perimetroTriangolo(2,3,4));
 
     }
 
